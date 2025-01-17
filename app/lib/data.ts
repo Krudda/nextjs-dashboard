a19ea7285d51db745057b746f2210a6cb9daf3c8
krudda <krudda@yandex.ru>
2025-01-17 08:43:05 +0300

    Add experimental partially prerendering model, install next-canary version

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -31,7 +31,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
